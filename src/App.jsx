b0e650d0c2181e4afadbbfd33ea22ed22526bfb0
Nurik <nurzhigit.turman@narxoz.kz>
2026-06-01 13:14:05 +0500

    Initial React project with CI

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import Welcome from "./components/Welcome";
+function App() {
+  return (
+    <>
+      <Welcome name="John" />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Welcome.jsx
@@ -0,0 +1,7 @@
+function Welcome({ name }) {
+    return (
+      <h1>Привет, {name}!</h1>
+    )
+  }
+  
+  export default Welcome
